1a95457ba4bddc354442ec49b8f5f87a6f6d50a2
Syun1208 <phamlong12082001@gmail.com>
2022-12-13 18:36:48 +0700

    Initial commit

--- run.sh
+++ run.sh
@@ -4,4 +4,17 @@ conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvi
 conda install scikit-learn
 pip install -r https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt
 pip install -r requirements.txt
+echo '-----------------------------DOWNLOADING WEIGHTS------------------------------------'
+#segmentation = './weights/segmentation'
+#detection = './weights/detection'
+#recognition = './weights/recognition'
+#url_segmentation =
+#url_detection =
+#url_recognition =
+#mkdir $segmentation
+#mkdir $detection
+#mkdir $segmentation
+#gdown
+#gdown
+#gdown
 python client.py
